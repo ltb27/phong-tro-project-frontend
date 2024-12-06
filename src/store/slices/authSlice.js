5ccb92b61a09ee8928efa85b663b3b04c975767b
ltb <letuanbao@mbg8.onmicrosoft.com>
2024-12-06 23:07:41 +0700

    refactor(auth): Separate auth logic from userSlice
    - Remove login/logout actions from userSlice
    - Add new authSlice with login/logout actions
    - Update initial state structure for authentication

---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,40 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import api from "@/services/api.js";
+
+// Initial state for the slice
+const initialState = {
+    isAuthenticated: false,
+    token: null,
+    refreshToken: null,
+    tokenExpiration: null,
+};
+
+// thunk actions
+export const logout = createAsyncThunk('user/logout', async () => {
+    await api.post('/api/auth/admin/logout');
+});
+
+export const login = createAsyncThunk('user/login', async (userData) => {
+    const response = await api.post('api/auth/admin/login', userData);
+    return response.data;
+});
+
+
+// Create the slice
+const SampleSlice = createSlice({
+    name: 'sample',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(login.fulfilled, (state, action) => {
+            state.userData = action.payload;
+        });
+        builder.addCase(logout.fulfilled, (state, action) => {
+            state.userData = {};
+        });
+    },
+});
+
+export const {} = SampleSlice.actions;
+
+export default SampleSlice
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,24 +1,9 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import api from "@/services/api.js";
+import {createSlice} from '@reduxjs/toolkit';
 
-// thunk actions
-export const logout = createAsyncThunk('user/logout', async () => {
-    await api.post('http://localhost:5173/api/auth/admin/logout');
-});
-
-export const login = createAsyncThunk('user/login', async (userData) => {
-    const response = await api.post('http://localhost:5173/api/auth/admin/login', userData);
-    return response.data;
-});
 
 // Initial state for the slice
 const initialState = {
-    userData: {
-        token: null,
-        expiration: null,
-        refreshTokenExpiration: null,
-        data: null,
-    },
+    userData: {},
 };
 
 // Create the slice
@@ -30,14 +15,7 @@ const UserSlice = createSlice({
             state.userData = action.payload;
         },
     },
-    extraReducers: builder => {
-        builder.addCase(login.fulfilled, (state, action) => {
-            state.userData = action.payload;
-        });
-        builder.addCase(logout.fulfilled, (state, action) => {
-            state.userData = {};
-        });
-    },
+  
 });
 
 export const {updateUserData} = UserSlice.actions;
